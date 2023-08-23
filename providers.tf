ce8f6e6ad83ee8bb4eeac55957e2bd4ceb7ed0c6
David Mastropolo <dave.mastropolo@gmail.com>
2023-08-23 16:14:55 -0700

    providers.tf test2

---
+++ providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  #region                  = "us-west-1"
+  # don't use us-west-1. Keep original AWS dev_node in region us-west-1
+  # run both setups simultaneously
+  region               = "us-west-2"
+  #shared_credentials_file = "~/.aws/credentials"
+  #profile                 = "vscode"
+}
